9487b4fb69db9e138514bf705c7b9425728eba1e
JulyCat274 <dds@7ya1.ru>
2024-09-23 20:28:42 +0400

    Д/з по модулю 6.3, v1

--- module_6_3.py
+++ module_6_3.py
@@ -1,20 +1,30 @@
 class Horse:
-    def __init__(self, x_distance = 0, sound = 'Frrr'):
+    def __init__(self, x_distance=0, sound='Frrr'):
         self.x_distance = x_distance
         self.sound = sound
 
     def run(self, dx):
         self.dx = dx
+        if dx > 0:
+            x_distance = self.x_distance + self.dx
+            return x_distance
+
 
 class Eagle:
-    def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat'):
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
         self.y_distance = y_distance
         self.sound = sound
 
     def fly(self, dy):
         self.dy = dy
-
+        if dy > 0:
+            y_distance = self.y_distance + self.dy
+            return y_distance
 class Pegasus(Horse, Eagle):
+    def __init__(self, x_distance, y_distance, sound):
+        Horse.__init__(self, x_distance)
+        Eagle.__init__(self, y_distance)
+        super().__init__(sound)
     def move(self, dx, dy):
         pass
 
@@ -25,13 +35,14 @@ class Pegasus(Horse, Eagle):
     def voice(self):
         pass
 
+
 p1 = Pegasus()
 
 print(p1.get_pos())
-p1.move(10, 15)
-print(p1.get_pos())
-p1.move(-5, 20)
-print(p1.get_pos())
+# p1.move(10, 15)
+# print(p1.get_pos())
+# p1.move(-5, 20)
+# print(p1.get_pos())
 
 p1.voice()
 
