fc0365743bf9ea94292e20c1622c38f87b9a020a
JulyCat274 <dds@7ya1.ru>
2024-09-25 10:00:54 +0400

    Д/з по модулю 6.3, final

--- module_6_3.py
+++ module_6_3.py
@@ -1,53 +1,50 @@
 class Horse:
-    def __init__(self, x_distance=0, sound='Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.dx = dx
-        if dx > 0:
-            x_distance = self.x_distance + self.dx
-            return x_distance
+        self.x_distance = self.x_distance + self.dx
+        return self.x_distance
 
 
 class Eagle:
-    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.dy = dy
-        if dy > 0:
-            y_distance = self.y_distance + self.dy
-            return y_distance
+        self.y_distance = self.y_distance + self.dy
+        return self.y_distance
+
+
 class Pegasus(Horse, Eagle):
-    def __init__(self, x_distance, y_distance, sound):
-        Horse.__init__(self, x_distance)
-        Eagle.__init__(self, y_distance)
-        super().__init__(sound)
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
     def move(self, dx, dy):
-        pass
+        self.dx = dx
+        self.dy = dy
+        Horse.run(self, dx)
+        Eagle.fly(self, dy)
+        return self.x_distance, self.y_distance
 
-
-    def get_pos(self): # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance)
-        pass
+    def get_pos(self):
+        return self.x_distance, self.y_distance
 
     def voice(self):
-        pass
+        print(f'{self.sound}')
 
 
 p1 = Pegasus()
 
 print(p1.get_pos())
-# p1.move(10, 15)
-# print(p1.get_pos())
-# p1.move(-5, 20)
-# print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
 
 p1.voice()
-
-# Вывод на консоль:
-# (0, 0)
-# (10, 15)
-# (5, 35)
-# I train, eat, sleep, and repeat
