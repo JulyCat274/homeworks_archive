197953d5e51ccdd14b912025aa415e609fd3c5cd
JulyCat274 <dds@7ya1.ru>
2024-10-12 14:12:01 +0400

    Д/з по модулю 7.1, final

--- module_7_2.py
+++ module_7_2.py
@@ -1,17 +1,15 @@
 def custom_write(file_name, strings):
-    strings_count = 0
+    strings_positions = dict()
+    string_count = 0
     file = open(file_name, 'w', encoding='utf-8')
     for string in strings:
-        strings_count += 1
-        strings = (strings_count, file.tell())
+        string_count += 1
+        key = (string_count, file.tell())
+        value = string
+        strings_positions[key] = value
         file.write(f'{string}\n')
-        my_dict = {strings:string}
-        print(my_dict)
     file.close()
-    file = open(file_name, 'r', encoding='utf-8')
-    result = file.read()
-    file.close()
-
+    return strings_positions
 
 
 info = [
@@ -19,15 +17,8 @@ info = [
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
-
-# Вывод на консоль:
-# ((1, 0), 'Text for tell.')
-# ((2, 16), 'Используйте кодировку utf-8.')
-# ((3, 66), 'Because there are 2 languages!')
-# ((4, 98), 'Спасибо!')
-# Как выглядит файл после запуска:
+    print(elem)
