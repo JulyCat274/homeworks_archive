86665b7bee7b04dcb9c65b536348a551caad87ab
JulyCat274 <dds@7ya1.ru>
2024-10-18 22:32:49 +0400

    Д/з по модулю 7.3, final

--- module_7_3.py
+++ module_7_3.py
@@ -1,5 +1,6 @@
 all_words = dict()
 
+
 class WordsFinder:
     def __init__(self, *file_names):
         self.file_names = file_names
@@ -8,22 +9,40 @@ class WordsFinder:
         punctuation_marks = [',', '.', '=', '!', '?', ';', ':', ' - ']
         for file_name in self.file_names:
             with open(file_name, 'r', encoding='utf-8') as f:
-                for line in f:
-                    for marks in punctuation_marks:
-                        line = line.replace(marks, '')
-                    key = file_name
-                    value = line.lower().split()
-                    all_words[key] = value
-                    print(all_words)
+                single_string = f.read()
+                for marks in punctuation_marks:
+                    single_string = single_string.replace(marks, '')
+        key = file_name
+        value = single_string.lower().split()
+        all_words[key] = value
+        return all_words
+
+    def find(self, word):
+        self.word = word
+        word_find = dict()
+        for file_name in self.file_names:
+            words_list = all_words.get(file_name)
+            my_word = word.lower()
+            words_count = words_list.index(my_word) + 1
+            key = file_name
+            value = words_count
+            word_find[key] = value
+            return word_find
+
+    def count(self, word):
+        self.word = word
+        word_count_dic = dict()
+        for file_name in self.file_names:
+            words_list = all_words.get(file_name)
+            my_word = word.lower()
+            word_count = words_list.count(my_word)
+            key = file_name
+            value = word_count
+            word_count_dic[key] = value
+            print(word_count_dic)
 
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-#print(finder2.find('TEXT')) # 3 слово по счёту
-#(finder2.count('teXT')) # 4 слова teXT в тексте всего
-
-
-
-#{'test_file.txt': ["it's", 'a', 'text', 'for', 'task', 'найти', 'везде', 'используйте', 'его', 'для', 'самопроверки', 'успехов', 'в', 'решении', 'задачи', 'text', 'text', 'text']}
-# {'test_file.txt': 3}
-# {'test_file.txt': 4}
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+(finder2.count('teXT'))  # 4 слова teXT в тексте всего
