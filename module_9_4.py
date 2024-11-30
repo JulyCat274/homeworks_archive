a9dc43d63b61b80d3c18aceb4b29bb55409f15bd
Дмитрий Дерендяев <dds@7ya1.ru>
2024-11-30 11:26:48 +0400

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,32 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+my_list = (list(map(lambda x, y: x == y, first, second)))
+print(my_list)
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + "\n")
+    return write_everything
+
+write = get_advanced_writer('example_9_4.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+from random import choice
+
+class MysticBall:
+    def __init__(self,words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall(['Первый', 'Второй', 'Третий', 'Четвёртый'])
+print(first_ball())
+print(first_ball())
+print(first_ball())
